3235c1fa659405b22a5f5661e6a7fd73962c5b28
Bryan McLellan <btm@chef.io>
2018-02-03 21:55:10 -0500

    Initial commit of code and docs
    Signed-off-by: Bryan McLellan <btm@chef.io>

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# emonmbus
-Arduino firmware for a dedicated mbus master for Emoncms
+# emonMbus
+Arduino firmware for a dedicated M-Bus master to submit "BTU" Heat Meter data to Emoncms
+
+This project is not supported or endorsed by the [OpenEnergyMonitor project](https://github.com/openenergymonitor), but is obviously completely dependent on it and heavily based on the project's hardware and software. Thanks go out to those folks for their Open Source work.
+
+## Hardware / Kit
+Some assembly required...
+
+- [Arduino Uno](https://store.arduino.cc/usa/arduino-uno-rev3)
+- JeeLabs [RFM12B Board](https://jeelabs.net/projects/hardware/wiki/RFM12B_Board) with a 433/434 MHz  
+  As far as I can tell, the [jeelib](https://github.com/jcw/jeelib) library does not support the RFM69HCW (high power) but does support the RFM69CW in compatibility mode as a replacement for the possibly deprecated RFM12B.
+  You need a radio supported by jeelib to be able to transmit to an emonBase or emonPi.  I used the board from [Modern Devices](https://moderndevice.com/product/jeelabs-rfm12b-board-kit/) with a 69C '434MHz' radio.
+- OpenEnergyMonitor [m-bus master](https://github.com/openenergymonitor/HeatpumpMonitor/tree/master/Hardware/v4) circuit.  
+  The circuit used in the OpenEnergyMonitor Heatpump Monitor was based off of Tomas Menzl's [libmbus design](https://github.com/rscada/libmbus/tree/master/hardware).
+  The M-Bus master schematic can be best seen in the lower right right of the the [Heatpump Monitor v4](https://raw.githubusercontent.com/openenergymonitor/HeatpumpMonitor/master/Hardware/v4/schematic.png) schematic.
+  - D4 and D5 go to the corresponding digital pins on the Arduino
+  - 1 and 2 of T9 go to the M-Bus connection of the meter which is polarity independent and does not need terminating resistors like Modbus.
+  - The XL6009E1 step-up converter used in the OpenEnergyMonitor design can be acquired through [Addicore](https://www.addicore.com/XL6009E1-Boost-Converter-p/ad456.htm) and provides an adequate 36V that M-Bus desires when fed 5V from the Arduino.
+  - I used a BC557 as a substitute for the BC307 transistor which wasn't readily available.
+- Spire Metering Technologies [280T-S Ultrasonic Heat Meter](https://www.spiremt.com/ultrasonic-heat-meter-280t.html)  
+  The Shenitech/SpireMT 280T appears to be an ancestor of the 280T-S and is likely compatible from the documentation I've seen.
+  The meter is designed to measure consumption but the supply and return sensors can be swapped to measure production instead.
+
+## Resources
+- The Meter Bus (M-Bus) [documentation](http://www.m-bus.com/mbusdoc/default.php) is essential
+- OpenEnergyMonitor Heatpump Monitor [build guide](https://github.com/openenergymonitor/HeatpumpMonitor/blob/master/heatpumpmonitor_build.md)
+- The [MBUS Reader](https://github.com/openenergymonitor/HeatpumpMonitor/tree/master/Firmware/Arduino/MBUS_Reader) Arduino firmware is the best place to start to verify your M-Bus master circuit and poke at the meter. I learned a lot about the meter's data format this way.
+
+## Development
+You'll need the [CustomSoftwareSerial](https://github.com/ledongthuc/CustomSoftwareSerial) and [jeelib](https://github.com/jcw/jeelib) libraries.
+
+OpenEnergyMonitor has been exploring a number of hardware configurations to add an M-Bus master circuit to, from a emonPi replacement to an addon to the emonTx. I decided that this configuration was the best way I could produce firmware that someone else might make use of. There's some refactoring to do to support other meters, but contributions are welcome.
+
+Bryan McLellan <btm@loftninjas.org>
+
+---
+
+![Arduino](hardware/images/Arduino-small.jpg)
---
+++ emonMbus.ino
@@ -0,0 +1,134 @@
+/*
+ * emoncms mbus node
+ * Based on OpenEnergyMonitor code by Trystan Lea, Glyn Hudson, and others
+ * https://github.com/openenergymonitor/HeatpumpMonitor
+ * Copyright 2018 Bryan McLellan <btm@loftninjas.org>
+ * License: GPLv3 https://www.gnu.org/licenses/gpl.txt
+ */
+
+#define DEBUG true
+
+// Serial interface used for mbus to allow use of 8E1 encoding
+#include <CustomSoftwareSerial.h>
+CustomSoftwareSerial* customSerial;
+
+#define MBUS_BAUD_RATE 2400
+#define MBUS_ADDRESS 1
+#define MBUS_TIMEOUT 1000 // milliseconds
+#define MBUS_DATA_SIZE 255
+#define MBUS_GOOD_FRAME true
+#define MBUS_BAD_FRAME false
+#define MBUS_RX_PIN 4
+#define MBUS_TX_PIN 5
+
+// OpenEnergyMonitor wireless communication
+#define RFM69_ENABLE 1 // disable radio for mbus development without sending data
+#define RF_freq RF12_433MHZ
+#define RF69_COMPAT 1 // set to 1 to use RFM69CW, must be before JeeLib include
+#include <JeeLib.h>   // make sure V12 (latest) is used if using RFM69CW
+
+const int nodeID = 12;
+const int networkGroup = 210;   // OpenEnergyMonitor group is 210
+
+unsigned long loop_start = 0;
+unsigned long last_loop = 0;
+bool firstrun = true;
+
+/* emonhub.conf configuration
+  [[12]]
+    nodename = emonmbus
+    [[[rx]]]
+       names = energy, flow, power, flowrate, supplyT, returnT
+       datacodes = l,l,l,l,h,h
+       scales = 1,1,1,1,0.01,0.01
+       units = kW,kW,m,mm,C,C
+ */
+typedef struct {
+  int energy, flow, power, flowrate, supplyT, returnT ;
+} PayloadTX;
+
+PayloadTX emon_data_tx;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("emonMbus startup"));
+
+  Serial.print(F("RF69: node: "));
+  Serial.print(nodeID);
+  Serial.print(F(" networkGroup: "));
+  Serial.println(networkGroup);
+  if (RFM69_ENABLE) rf12_initialize(nodeID, RF_freq, networkGroup);
+
+  Serial.println(F("mbus:"));
+  Serial.print(F("  slave address: "));
+  Serial.println(MBUS_ADDRESS);
+  Serial.print(F("  baud rate: "));
+  Serial.println(MBUS_BAUD_RATE);
+  customSerial = new CustomSoftwareSerial(MBUS_RX_PIN, MBUS_TX_PIN); // rx, tx
+  customSerial->begin(MBUS_BAUD_RATE, CSERIAL_8E1); // mbus uses 8E1 encoding
+  delay(1000); // let the serial initialize, or we get a bad first frame
+}
+
+void loop() {
+  loop_start = millis();
+  
+  if (RFM69_ENABLE && rf12_recvDone() && rf12_crc == 0 && (rf12_hdr & RF12_HDR_CTL) == 0)
+  {
+    int node_id = (rf12_hdr & 0x1F);
+    byte n = rf12_len;
+    if (DEBUG) {
+      Serial.print(F("RFM node: "));
+      Serial.print(node_id);
+      Serial.print(F(" len:"));
+      Serial.println(n);
+    }
+  }
+
+  /************************
+   * DATA COLLECTION LOOP *
+   ************************/
+  if ((loop_start-last_loop)>=9800 || firstrun) { // 9800 = ~10 seconds
+    last_loop = loop_start; firstrun = false;
+
+    /*************
+     * MBUS DATA *
+     *************/
+    bool mbus_good_frame = false;
+    byte mbus_data[MBUS_DATA_SIZE] = { 0 };
+
+    if (DEBUG) Serial.print(F("mbus: requesting data from address: "));
+    if (DEBUG) Serial.println(MBUS_ADDRESS);
+    mbus_request_data(MBUS_ADDRESS);
+    mbus_good_frame = mbus_get_response(mbus_data, sizeof(mbus_data));
+    if (mbus_good_frame) {
+      if (DEBUG) Serial.println(F("mbus: good frame: "));
+      if (DEBUG) print_bytes(mbus_data, sizeof(mbus_data));
+      
+      // SMTenergy 6h 27b, SMTflowrate 3Bh 44b, SMTflowT 59h 49b, SMTreturnT 5Dh 53b, SMTflow 14h 33b, SMTheat 2Ch 39b   
+      emon_data_tx.energy = get_spire_value(mbus_data, 27, 4);
+      emon_data_tx.flow = get_spire_value(mbus_data, 33, 4);
+      emon_data_tx.power = get_spire_value(mbus_data, 39, 3);
+      emon_data_tx.flowrate = get_spire_value(mbus_data, 44, 3);
+      emon_data_tx.supplyT = get_spire_value(mbus_data, 49, 2);
+      emon_data_tx.returnT = get_spire_value(mbus_data, 53, 2);
+      Serial.print(F("SMTenergy kWh: ")); Serial.println(emon_data_tx.energy);
+      Serial.print(F("SMTflow: ")); Serial.println(emon_data_tx.flow);
+      Serial.print(F("SMTpower: ")); Serial.println(emon_data_tx.power);
+      Serial.print(F("SMTflowrate: ")); Serial.println(emon_data_tx.flowrate);
+      Serial.print(F("SMTsupplyT: ")); Serial.println(emon_data_tx.supplyT);
+      Serial.print(F("SMTreturnT: ")); Serial.println(emon_data_tx.returnT);
+
+      send_rf_data();
+    } else {
+      Serial.print(F("mbus: bad frame: "));
+      print_bytes(mbus_data, sizeof(mbus_data));
+    }
+  }
+}
+
+void send_rf_data()
+{
+  rf12_sleep(RF12_WAKEUP);
+  rf12_sendNow(0, &emon_data_tx, sizeof(emon_data_tx));
+  rf12_sendWait(2);
+}
